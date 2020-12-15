ecd993293eca7d2ec2f9a0c25617c26240fc88af
Brendan O Brien <L00161967@student.lyit.ie>
2020-12-15 23:51:51 +0000

    Uncommented the correct command to continue onto the next part of the code

--- networkingPS_Brkn.ps1
+++ networkingPS_Brkn.ps1
@@ -88,7 +88,7 @@ function Test-Network
     }    Process
     {    #BSC DCM 2020, I need to send the list of $computerNames to the next part of the process (Foreach). 
     #Which command should I use?
-    #  Write-Output $computerNames  
+       Write-Output $computerNames  
     #  Write-Host $computerNames
     # Uncomment the correct one of the above choices!
 
