a6144115099b1533fed460d2678251e6ede46a52
L00162284 <L00162284@student.lyit.ie>
2020-12-18 21:17:08 +0000

    Function Get-OpenPorts Modified and Tested

--- networkingPS_Brkn.ps1
+++ networkingPS_Brkn.ps1
@@ -319,16 +319,19 @@ Function Get-NetworkInfo
 #Region Check-OpenPorts
 <#
 .SYNOPSIS
-   
+    Get Open Ports 
+
 .DESCRIPTION
-   
-.PARAMETERS    
+	This Function will check for OpenPors within ComputerName and PortList
+	
+.PARAMETERS  
+	$ComputerName: A Valid Computer Name or IP Address
 #>
 
 # BSc DCM - fix this
 # fill in appropriate comments for the method as per the section above. this comment refers to the 
 # check-openports Function shown below.
-Function Check-OpenPorts
+Function Get-OpenPorts
 {
     [CmdletBinding()]
     [Alias()]
@@ -339,38 +342,31 @@ Function Check-OpenPorts
         $ComputerName,
         [Parameter()]
         [string[]]
-        $PortList
-        )
-    $checkOpenPortsArray = @()
+        $PortList)
+		$checkOpenPortsArray = @()
     try
     {
-        # BSc DCM 2020 - fix this
-        # We need an iterator here to go through all $ports in $PortList
-        # Write in the single line of code to iterate through the port list
-        {
-            
-            #BSc DCM 2020 - Fix this
-            # $portConnected =
-            # finish the above line of code using the Test-NetConnection command and then uncomment.
-            #check by port $port, and the computer name $ComputerName.
-            # add an action of SilentlyContinue if a warning occurs
-            # this is one line of code only!
-            $ports = [ordered]@{
-                ComputerName=$ComputerName
-                Port=$port
-                Open=$portConnected.TcpTestSucceeded
-            }
-            $checkOpenPortsArray += New-Object -TypeName PSObject -Property $ports
+        # below an iterator here to go through all $ports in $PortList
+        Foreach ($ports in $PortList)
+		{
+        $portConnected = Test-NetConnection -InformationLevel Detailed -ComputerName $computerName -port $port -Warning SilentlyContinue
+        #check by port $port, and the computer name $ComputerName, added an action of SilentlyContinue if a warning occurs
+        $ports = [ordered]@{
+        ComputerName = $ComputerName
+        Port = $port
+        Open = $portConnected.TcpTestSucceeded
+                           }
+        $checkOpenPortsArray += New-Object -TypeName PSObject -Property $ports
         }
     }
     catch 
     { 
         $ports = [ordered]@{
-                ComputerName=$ComputerName
-                Port=$port
-                Open="(Check-OpenPorts) Server Error: " + $_.Exception.Message + " : "  + $_.FullyQualifiedErrorId
-            }
-            $checkOpenPortsArray = New-Object -TypeName PSObject -Property $ports
+        ComputerName = $ComputerName
+        Port = $port
+        Open = "(Check-OpenPorts) Server Error: " + $_.Exception.Message + " : "  + $_.FullyQualifiedErrorId
+                           }
+        $checkOpenPortsArray = New-Object -TypeName PSObject -Property $ports
     }
     return $checkOpenPortsArray   
 }
