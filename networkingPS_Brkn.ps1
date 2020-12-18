f48a6dd3dd27de59f3d3c50173baf02b5f7b3f4e
L00162284 <L00162284@student.lyit.ie>
2020-12-18 18:40:14 +0000

    Function Get-WarningErrors Corrected and Tested

--- networkingPS_Brkn.ps1
+++ networkingPS_Brkn.ps1
@@ -186,17 +186,18 @@ Function Get-UserDetail
 }
 #endRegion
 
-#Region Check-warningsErrors
+#Region Check-WarningsErrors
 <#
 .SYNOPSIS
-   Check for warnings or errors 
+    Check for warnings or errors 
+	
 .DESCRIPTION
-   This Function will check if any warnings or errors is on the server EventLog
+    This Function will check if any warnings or errors is on the server EventLog
 
 .PARAMETERS
     $ComputerName: A Valid Computer Name or IP Address
 #>
-Function Check-WarningsErrors
+Function Get-WarningsErrors
 {
     [CmdletBinding()]
     [Alias()]
@@ -205,34 +206,32 @@ Function Check-WarningsErrors
         [Parameter()]
         [string]
         $ComputerName
-        )
-
+         )
     # Date before and after to check 24 hours worth of data
     $DateBefore = (Get-Date)
     $DateAfter = (Get-Date).AddDays(-1)
-
     $errorOutputArray = @()
     try
     {
         # Check if any security errors or warning was log to the eventlog
         $EventLogTest = Get-EventLog -ComputerName $ComputerName -LogName Security -Before $DateBefore -After $DateAfter | Where-Object {$_.EntryType -like 'Error' -or $_.EntryType -like 'Warning'}
-
         #$EventLogTest = Get-EventLog -LogName System -Newest 5   @TEST
-        If ($EventLogTest -ne $null)
+        if ($null -ne $EventLogTest)
         {
             # If Warnings or Errors found, then write it out to the log file
             Foreach ($eventLog in $EventLogTest)
             {
                 $errorOutput = [ordered]@{
-                    ComputerName=$ComputerName
-                    EntryType = $eventLog.EntryType
-                    Index = $eventLog.Index 
-                    Source = $eventLog.Source
-                    InstanceID = $eventLog.InstanceID
-                    Message = $eventLog.Message }
-                    $errorOutputArray = New-Object -TypeName PSObject -Property $errorOutput
+                ComputerName=$ComputerName
+                EntryType = $eventLog.EntryType
+                Index = $eventLog.Index 
+                Source = $eventLog.Source
+                InstanceID = $eventLog.InstanceID
+                Message = $eventLog.Message}
+                $errorOutputArray = New-Object -TypeName PSObject -Property $errorOutput
             }
-        }else
+        }
+		else
         {
             # If no errors where found
                 $errorOutput = [ordered]@{
@@ -248,11 +247,11 @@ Function Check-WarningsErrors
     catch 
     { 
         $errorOutput = [ordered]@{
-                ComputerName=$ComputerName
-                EntryType = "" ;  Index = "" ; Source = ""
-                InstanceID = ""
-                Message = "(Check-WarningsErrors) Server Error: " + $_.Exception.Message + " : "  + $_.FullyQualifiedErrorId }
-                $errorOutputArray = New-Object -TypeName PSObject -Property $errorOutput
+            ComputerName=$ComputerName
+            EntryType = "" ;  Index = "" ; Source = ""
+            InstanceID = ""
+            Message = "(Check-WarningsErrors) Server Error: " + $_.Exception.Message + " : "  + $_.FullyQualifiedErrorId }
+            $errorOutputArray = New-Object -TypeName PSObject -Property $errorOutput
 
     }
     return $errorOutputArray   
